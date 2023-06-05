e204280dc406e413fd283c6a452596cdf33de8dd
DenGrachev59 <2717087@mail.ru>
2023-06-05 18:43:14 +0500

    второе дз выполнено

--- tests/test_item.py
+++ tests/test_item.py
@@ -14,3 +14,22 @@ def test_apply_discount():
     object_ = Item('Name', 10, 50)
     object_.apply_discount()
     assert object_.price == 5
+
+def test_set_name():
+    object_ = Item('Бур', 5, 10)
+    object_.name = 'Тур'
+    assert object_.name == 'Тур'
+    object_.name = 'Суперсмартфон'
+    assert object_.name == 'Тур'
+    object_.name = '141'
+    assert object_.name == 'Тур'
+    object_.name = 150
+    assert object_.name == 'Тур'
+
+def test_string_to_number():
+    assert Item.string_to_number('15.6') == 15
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all)==5
+
